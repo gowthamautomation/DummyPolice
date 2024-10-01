30cfb510af59bf8c4f9392d1212e4cf782c916a2
gowthamautomation <gowthammech444@gmail.com>
2024-10-01 16:18:51 +0530

    g

--- src/test/java/Summa.java
+++ src/test/java/Summa.java
@@ -1,4 +1,32 @@
 
 public class Summa {
+	public static void main(String[]args) {
+
+		String s="Gowtham";
+		
+		int vowel=0;
+		int novowel=0;
+		
+		String lc = s.toLowerCase();
+		for (int i = 0; i < s.length(); i++) {
+			char f=s.charAt(i);
+			
+			if (f=='a'||f=='e'||f=='i'||f=='o'||f=='u') {
+				vowel++;
+			}
+			else{
+				novowel++;
+			}
+		}
+	
+		System.out.println("vowels "+vowel);
+		System.out.println("No vowels "+novowel);
+		
+		
+		
+		
+		
+	
+	}
 
 }
